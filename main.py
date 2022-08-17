b6b239c3964bc841a1655c414580052b4a8584b5
Thibaud <62288874+thibaudmaciuk@users.noreply.github.com>
2022-08-17 08:59:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import telegram_send
 from datetime import datetime
+import time
 
 anniversaire = 1
 day = datetime.now().strftime('%A')
 year = datetime.now().strftime('%Y')
 date = datetime.now().strftime('%d-%m')
 Heure = datetime.now().strftime('%H:%M:%S')
+weather = "none"
 
 #print(f"La date est le {date}")
 #print(f"L'heure est {Heure}")
@@ -15,11 +17,16 @@ Heure = datetime.now().strftime('%H:%M:%S')
 
 
 while True:
+
+    time.sleep(3)
+    
+    
+    
     if date == '18-08':
         if anniversaire == 0:
             telegram_send.send(messages=[f"Bon anniversaire monsieur pour ce {2022-2000}eme anniversaire"])
             anniversaire = 1
     if date == '19-08':
         anniversaire = 1
-    elif :
-        print("Aujourd'hui on est pas le 16 aout")
+#    else:
+#        print("Aujourd'hui on est pas le 16 aout")
